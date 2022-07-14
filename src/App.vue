80c286f94bb13113a684ce564c0789d2ae609735
PedroGzzp <pedroluisgzzp@gmail.com>
2022-07-14 07:43:47 -0500

    Tabla CDN a CLI

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="wrapper_switch" :style="textoDerecha">
+      Modo Noche <img src="img/luna.svg" width="15px" height="15px">
+      <label class="switch">
+      <input type="checkbox" @click="cambiarVista()">
+      <span class="slider round"></span>
+      </label>
+    </div>
+    <miTabla
+      :lista="elenco"
+      :darkmode="darkmode"
+    ></miTabla>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import miTabla from './components/miTabla.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    miTabla
+  },
+  data(){
+    return{
+      elenco: [
+                  {
+                      id: 1,
+                      nombre: 'Leonardo DiCaprio',
+                      personaje: 'Rick Dalton'
+                  },
+                  {
+                      id: 2,
+                      nombre: 'Brad Pitt',
+                      personaje: 'Cliff Booth'
+                  },
+                  {
+                      id: 3,
+                      nombre: 'Margot Robbie',
+                      personaje: 'Sharon Tate'
+                  },
+                  {
+                      id: 4,
+                      nombre: 'Margaret Qualley',
+                      personaje: '«Pussycat»'
+                  }
+          ],
+          darkmode: false,
+          textoDerecha: {'text-align' : 'right'}
+    }
+  },
+  methods: {
+        cambiarVista(){
+            this.darkmode = !this.darkmode 
+        }
+    }
 }
 </script>
 
---
+++ src/components/miTabla.vue
@@ -0,0 +1,34 @@
+<template>
+  <table :class="{'table table-striped table-bordered' : !darkmode, 'table table-striped table-bordered table-dark' : darkmode}">
+            <thead>
+                <tr>
+                    <th colspan="2" class="text-center">Elenco</th>
+                </tr>
+                <tr>
+                    <th>Actor/Actriz</th>
+                    <th>Personaje</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="items in lista" :key="items.id">
+                    <td>{{items.nombre}}</td>
+                    <td>{{items.personaje}}</td>
+                </tr>
+            </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'miTabla',
+  props: {
+    lista: Array,
+    darkmode: Boolean
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
